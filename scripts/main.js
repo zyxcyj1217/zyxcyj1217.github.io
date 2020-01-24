4d52c8b6c816ecf9254ee423bef5a1467dd171fe
zyxcyj1217 <zyxcyj1217@gmail.com>
2020-01-24 09:45:12 +0800

    website

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 var textIndex = 0;
-
-var text1 = [
-  "注入神奇能量中...&nbsp", "注入神奇能量中..._"
-];
 var text0 = [
+//  "注入神奇能量中...&nbsp", "注入神奇能量中..._"
+  "新年快樂！！！&nbsp", "新年快樂！！！_"
+];
+var text1 = [
   "ㄓ_", "ㄓㄨ_", "注_",
   "注ㄖ_", "注ㄖㄨ_", "注入_",
   "注入ㄕ_", "注入ㄕㄣ_", "注入神_",
@@ -13,24 +13,30 @@ var text0 = [
   "注入神奇能量ㄓ_", "注入神奇能量ㄓㄨ_", "注入神奇能量ㄓㄨㄥ_", "注入神奇能量中_",
   "注入神奇能量中._", "注入神奇能量中.._", "注入神奇能量中..._",
 ];
-
+var text2 = [
+  "ㄒ_", "ㄒㄧ_", "ㄒㄧㄣ_", "新_",
+  "新ㄋ_", "新ㄋㄧ_", "新ㄋㄧㄢ_", "新年_",
+  "新年ㄎ_", "新年ㄎㄨ_", "新年ㄎㄨㄞ_", "新年快_",
+  "新年快ㄌ_", "新年快ㄌㄜ_", "新年快樂_",
+  "新年快樂！_", "新年快樂！！_", "新年快樂！！！_",
+]
 function textChange(index) {
 
   if(index == 0) {
 
-    document.getElementById("TEXT").innerHTML = text0[textIndex];
+    document.getElementById("TEXT").innerHTML = text2[textIndex];
 
-    if(textIndex < 25) {
+    if(textIndex < 17) {
       textIndex++;
     }else{
       textIndex = 0;
-      clearInterval(timer);
+      clearInterval(timer0);
       setInterval(function(){textChange(1)},480);
     }
 
   }else{
 
-    document.getElementById("TEXT").innerHTML = text1[textIndex];
+    document.getElementById("TEXT").innerHTML = text0[textIndex];
 
     if(textIndex < 1) {
       textIndex++;
@@ -43,11 +49,9 @@ function textChange(index) {
 }
 
 var menuIndex = 0;
-
 var menu = [
   "嘣", "吧", "啦", "嘟", "吧", "啦",
 ];
-
 function menuChange(index) {
 
   document.getElementById("MENU").innerHTML = menu[menuIndex];
